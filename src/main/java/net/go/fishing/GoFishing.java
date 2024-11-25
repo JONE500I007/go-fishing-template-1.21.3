38e2c792947844a1be91dfb211b240c849415094
JONE 500 <Ingkawat2023Reals@gmail.com>
2024-11-26 00:30:58 +0700

    add item and block

--- src/main/java/net/go/fishing/GoFishing.java
+++ src/main/java/net/go/fishing/GoFishing.java
@@ -2,6 +2,9 @@ package net.go.fishing;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.go.fishing.block.ModBlocks;
+import net.go.fishing.item.ModItemGroups;
+import net.go.fishing.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +18,9 @@ public class GoFishing implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
